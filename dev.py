d4b924727f3f3daf7484fbc9f9b016102a3f9ad6
Ron Gorodetzky <rongoro@gmail.com>
2018-12-03 03:07:07 -0800

    start over with dev implementation

--- dev.py
+++ dev.py
@@ -1,21 +1,74 @@
 #!/usr/bin/env python
+"""Dev repository tool
 
+The goal of this tool is to support a unified development environment for an 
+organization. 
+
+Development notes:
+
+As much as possible this system is designed to work with python 2.7 and be a 
+single file that can just be copied or symlinked into place and not require 
+any installation.
+"""
 from __future__ import print_function
 
+import ConfigParser
+import subprocess
 import os
 
-### start Dev repo helpers
 
-def find_dev_root(curdir):
-    working_dir = curdir
-    while True:
-        test_location = os.path.join(working_dir, 'DEV_ROOT')
+class Repo(object):
+    @staticmethod
+    def get_dev_root(curdir):
+        """Find the root of the Dev repo"""
 
-        if os.path.exists(test_location):
-            return working_dir
-        if working_dir == '/':
-            raise Exception('Could not find DEV_ROOT')
-        working_dir = os.path.dirname(working_dir)
+        working_dir = curdir
+        while True:
+            test_location = os.path.join(working_dir, 'DEV_ROOT')
 
-### end Dev repo helpers
+            if os.path.exists(test_location):
+                return working_dir
+            if working_dir == '/':
+                raise Exception('Could not find DEV_ROOT')
+            working_dir = os.path.dirname(working_dir)
 
+    @staticmethod
+    def get_dev_config(directory):
+        """Get the Dev configuration for the given package"""
+
+        if not os.path.exists(os.path.join(os.path.dirname(directory), 'DEV')):
+            raise Exception("DEV config doesn't exist for %s" % directory)
+
+        config = ConfigParser.SafeConfigParser()
+        config.read(os.path.join(os.path.dirname(directory), 'DEV'))
+
+        return config
+   
+
+class Commands(object):
+    @staticmethod
+    def run_command(command, replace_current_term=False, capture_output=False):
+        if replace_current_term:
+            os.execv(command[0], command)
+        else:
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
+
+            output = []
+            for stdout_line in iter(process.stdout.readline, ""):
+                if capture_output:
+                    output.append(stdout_line.strip())
+                else:
+                    print(stdout_line, end='')
+
+            process.stdout.close()
+            return_code = process.wait()
+            if return_code:
+                raise subprocess.CalledProcessError(return_code, command)
+        return output
+
+
+class DockerHelpers(object):
+    pass
--- dev_test.py
+++ dev_test.py
@@ -5,11 +5,27 @@ import unittest
 test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data')
 
 class DevRepoHelpersTests(unittest.TestCase):
-    def test_find_dev_root(self):
+    def test_get_dev_root(self):
         self.assertEqual(os.path.join(test_data_dir, 'test_root'),
-            dev.find_dev_root(os.path.join(test_data_dir, 'test_root')))
+            dev.Repo.get_dev_root(os.path.join(test_data_dir, 'test_root')))
 
-        self.assertRaises(Exception, dev.find_dev_root, '/usr/bin')
+    def test_get_dev_root_not_exist(self):
+        self.assertRaises(Exception, dev.Repo.get_dev_root, '/usr/bin')
+
+    def test_get_dev_config(self):
+        self.assertTrue(dev.Repo.get_dev_config(
+            os.path.join(test_data_dir, 'test_root', 'world', 'example.com', 'project_foo')
+        ))
+
+    def test_get_dev_config_not_exist(self):
+        self.assertRaises(Exception, dev.Repo.get_dev_config, 
+                          os.path.join(test_data_dir, 'test_root', 'world', 'unconfigured.com', 'project_unconifigured'))
+
+class DevShellHelpersTests(unittest.TestCase):
+    def test_run_command(self):
+        self.assertEqual(['Test Success!'],
+        dev.Commands.run_command(['/bin/echo', 'Test Success!'], capture_output=True))
+
 
 if __name__ == '__main__':
     unittest.main()
