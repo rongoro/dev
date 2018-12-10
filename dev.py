67bf52ea0dfb99f4190ce407fb9ab9e5f97e93a7
Ron Gorodetzky <rongoro@gmail.com>
2018-12-10 04:13:20 -0800

    add dev config helpers

--- dev.py
+++ dev.py
@@ -16,8 +16,14 @@ import ConfigParser
 import subprocess
 import os
 import socket
+import json
+import re
 
 from contextlib import closing
+
+
+class DevRepoException(Exception):
+    pass
 
 
 class Repo(object):
@@ -27,33 +33,122 @@ class Repo(object):
 
         working_dir = curdir
         while True:
-            test_location = os.path.join(working_dir, 'DEV_ROOT')
+            test_location = os.path.join(working_dir, "DEV_ROOT")
 
             if os.path.exists(test_location):
                 return working_dir
-            if working_dir == '/':
-                raise Exception('Could not find DEV_ROOT')
+            if working_dir == "/":
+                raise Exception("Could not find DEV_ROOT")
             working_dir = os.path.dirname(working_dir)
 
     @staticmethod
     def get_dev_config(directory):
         """Get the Dev configuration for the given package"""
 
-        if not os.path.exists(os.path.join(os.path.dirname(directory), 'DEV')):
+        if not os.path.exists(os.path.join(os.path.dirname(directory), "DEV")):
             raise Exception("DEV config doesn't exist for %s" % directory)
 
         config = ConfigParser.SafeConfigParser()
-        config.read(os.path.join(os.path.dirname(directory), 'DEV'))
+        config.read(os.path.join(os.path.dirname(directory), "DEV"))
 
         return config
 
 
-class Runtime(object):
+class ConfigHelpers(object):
+    @staticmethod
+    def parse_config(config_file):
+        """Reads a standard ini config and converts it to a dev dictionary
+        
+        What it does is takes an
+        """
+
+
+class GlobalConfig(object):
+    @staticmethod
+    def get(dev_tree):
+        dev_root = Repo.get_dev_root(dev_tree)
+        with open(os.path.join(dev_root, "DEV_ROOT")) as f:
+            global_config = json.load(f)
+        return global_config
 
     @staticmethod
+    def get_runtimes(dev_tree):
+        config = GlobalConfig.get(dev_tree)
+        return sorted(config["runtimes"].keys())
+
+
+class ProjectConfig(object):
+    @staticmethod
+    def parse_project_path(dev_tree, project_path, require_project_name=True):
+        if not project_path.startswith("//"):
+            raise DevRepoException("Project path must start with //")
+
+        parts = re.match("//(?P<path>[^:]*)(?P<project>:[A-Za-z0-9_-]+)?", project_path)
+        if not parts:
+            raise DevRepoException("Bad project path: %s" % project_path)
+
+        project_parent_dir = os.path.join(dev_tree, parts.groupdict()["path"])
+        project_name = parts.groupdict()["project"]
+
+        if require_project_name and not project_name:
+            raise DevRepoException("No project name specified.")
+        elif not require_project_name and (project_name is not None):
+            raise DevRepoException("Project should not be specified.")
+
+        # strip the colon from the project name
+        if project_name:
+            project_name = project_name[1:]
+
+        return project_parent_dir, project_name
+
+    @staticmethod
+    def get(dev_tree, project_path):
+        project_parent_dir, project_name = ProjectConfig.parse_project_path(
+            dev_tree, project_path
+        )
+
+        with open(os.path.join(project_parent_dir, "DEV")) as f:
+            full_config = json.load(f)
+
+        if project_name not in full_config:
+            raise DevRepoException(
+                "Project %s doesn't exist at %s" % (project_name, project_parent_dir)
+            )
+
+        return full_config[project_name]
+
+    @staticmethod
+    def list_projects(dev_tree, project_path):
+        project_parent_dir, project_name = ProjectConfig.parse_project_path(
+            dev_tree, project_path, require_project_name=False
+        )
+
+        with open(os.path.join(project_parent_dir, "DEV")) as f:
+            full_config = json.load(f)
+
+        return sorted(full_config.keys())
+
+    @staticmethod
+    def get_commands(dev_tree, project_path):
+        project_parent_dir, project_name = ProjectConfig.parse_project_path(
+            dev_tree, project_path
+        )
+
+        with open(os.path.join(project_parent_dir, "DEV")) as f:
+            full_config = json.load(f)
+
+        if "commands" not in full_config[project_name]:
+            return []
+            
+        return full_config[project_name]["commands"]
+
+class Runtime(object):
+    @staticmethod
     def setup(location):
-        if not (os.path.exists(os.path.join(location, 'bin'))
-                and os.path.isdir(os.path.join(location, 'bin'))):
+        if not (
+            os.path.exists(os.path.join(location, "bin"))
+            and os.path.isdir(os.path.join(location, "bin"))
+        ):
             return False
         else:
             return True
@@ -65,29 +160,27 @@ class Runtime(object):
             with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
                 sock.settimeout(1)
                 try:
-                    res = sock.bind(('localhost', port))
+                    res = sock.bind(("localhost", port))
                 except Exception as e:
-                    if e.errno is 98: ## Errorno 98 means address already bound
+                    if e.errno is 98:  ## Errorno 98 means address already bound
                         continue
             ports.append(port)
             if len(ports) == count:
                 break
         return ports
 
-
     @staticmethod
     def run_command(command, capture_output=False):
         process = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
+            command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
 
         output = []
         for stdout_line in iter(process.stdout.readline, ""):
             if capture_output:
                 output.append(stdout_line.strip())
             else:
-                print(stdout_line, end='')
+                print(stdout_line, end="")
 
         process.stdout.close()
         return_code = process.wait()
@@ -95,27 +188,31 @@ class Runtime(object):
             raise subprocess.CalledProcessError(return_code, command)
         return output
 
+
 class DockerRuntime(Runtime):
     @staticmethod
     def setup(location, name):
-        output = Runtime.run_command(['docker', 'build', '-t', name, location], capture_output=True)
-        return output[-1].startswith('Successfully tagged ' + name)
+        output = Runtime.run_command(
+            ["docker", "build", "-t", name, location], capture_output=True
+        )
+        return output[-1].startswith("Successfully tagged " + name)
 
     @staticmethod
     def run_command(name, command, capture_output=False):
-        output = Runtime.run_command(['docker', 'run', '-it', name] + command, capture_output=capture_output)
+        output = Runtime.run_command(
+            ["docker", "run", "-it", name] + command, capture_output=capture_output
+        )
         if capture_output == True:
             return output
 
     @staticmethod
     def get_images():
-        output = Runtime.run_command(['docker', 'images'], capture_output=True)
+        output = Runtime.run_command(["docker", "images"], capture_output=True)
         return [l.split()[0] for l in output[1:]]
 
     @staticmethod
     def rm_image(image_name):
-        output = Runtime.run_command(['docker', 'rmi', '-f', image_name], capture_output=True)
+        output = Runtime.run_command(
+            ["docker", "rmi", "-f", image_name], capture_output=True
+        )
         return output
-
-class DockerHelpers(object):
-    pass
--- dev_test.py
+++ dev_test.py
@@ -3,11 +3,13 @@ import os
 import subprocess
 import unittest
 import socket
+import json
 
 from contextlib import closing
 
 
 test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data")
+test_root = os.path.join(test_data_dir, "test_root")
 
 
 class DevRepoHelpersTests(unittest.TestCase):
@@ -43,6 +45,83 @@ class DevRepoHelpersTests(unittest.TestCase):
         )
 
 
+class DevConfigHelpersTest(unittest.TestCase):
+    maxDiff = None
+
+    def test_get_global_config(self):
+        self.assertEqual(
+            {
+                "version": "1",
+                "runtimes": {
+                    "base": {"type": "docker", "project": "//runtimes:test_runtime"},
+                    "bad_runtime": {
+                        "type": "docker",
+                        "project": "//runtimes:does_not_exist",
+                    },
+                },
+            },
+            dev.GlobalConfig.get(test_root),
+        )
+
+    def test_list_available_runtimes(self):
+        self.assertEqual(
+            ["bad_runtime", "base"], dev.GlobalConfig.get_runtimes(test_root)
+        )
+
+    def test_get_project_list(self):
+        self.assertEqual(
+            [
+                "project_bar",
+                "project_bar_no_commands",
+                "project_foo",
+                "project_foo_other",
+            ],
+            dev.ProjectConfig.list_projects(test_root, "//world/example.com"),
+        )
+
+    def test_get_project_commands(self):
+        self.assertEqual(
+            {"build": "echo foo"},
+            dev.ProjectConfig.get_commands(
+                test_root, "//world/example.com:project_foo"
+            ),
+        )
+
+        self.assertEqual(
+            [],
+            dev.ProjectConfig.get_commands(
+                test_root, "//world/example.com:project_bar_no_commands"
+            ),
+        )
+
+    def test_get_project_config(self):
+        self.assertRaises(
+            dev.DevRepoException,
+            dev.ProjectConfig.get,
+            test_root,
+            "bad_path/foo_project:bad",
+        )
+
+        self.assertRaises(
+            dev.DevRepoException,
+            dev.ProjectConfig.get,
+            test_root,
+            "//bad_project_path_with_no_colon",
+        )
+
+        self.assertRaises(
+            dev.DevRepoException,
+            dev.ProjectConfig.get,
+            test_root,
+            "//world/example.com:project_not_exist",
+        )
+
+        self.assertEqual(
+            {"path": "project_foo", "commands": {"build": "echo foo"}},
+            dev.ProjectConfig.get(test_root, "//world/example.com:project_foo"),
+        )
+
+
 class LocalRuntimeTests(unittest.TestCase):
     def test_run_command(self):
         self.assertEqual(
@@ -58,7 +137,7 @@ class LocalRuntimeTests(unittest.TestCase):
         with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
             sock.settimeout(0)
             free_port = 30002
-            res = sock.bind(("localhost", free_port))
+            sock.bind(("localhost", free_port))
             self.assertEqual([30003, 30004], dev.Runtime.find_open_ports(30002, 2))
 
     def test_local_runtime_setup(self):
